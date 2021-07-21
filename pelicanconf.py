62b4abfe515c3eb1d00a9e9cb9bfa1959c333ef9
Bryan Weber <bryan.w.weber@gmail.com>
2021-07-21 11:00:14 -0400

    Run black on pelicanconf.py

--- pelicanconf.py
+++ pelicanconf.py
@@ -1,5 +1,6 @@
 import sys
-sys.path.append('bulrush')
+
+sys.path.append("bulrush")
 
 import bulrush  # noqa: E402
 
@@ -8,15 +9,15 @@ import bulrush  # noqa: E402
 # LOAD_CONTENT_CACHE = False
 ########
 
-AUTHOR = 'Bryan Weber'
-SITENAME = 'Bryan Weber'
-SITEURL = ''
+AUTHOR = "Bryan Weber"
+SITENAME = "Bryan Weber"
+SITEURL = ""
 
-PATH = 'content'
+PATH = "content"
 
-TIMEZONE = 'America/New_York'
+TIMEZONE = "America/New_York"
 
-DEFAULT_LANG = 'en'
+DEFAULT_LANG = "en"
 
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
@@ -25,10 +26,10 @@ TRANSLATION_FEED_ATOM = None
 AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
-PAGE_URL = '{slug}/'
-PAGE_SAVE_AS = '{slug}/index.html'
-ARTICLE_URL = 'writing/{category}/{date:%Y}/{date:%m}/{date:%d}/{slug}/'
-ARTICLE_SAVE_AS = 'writing/{category}/{date:%Y}/{date:%m}/{date:%d}/{slug}/index.html'
+PAGE_URL = "{slug}/"
+PAGE_SAVE_AS = "{slug}/index.html"
+ARTICLE_URL = "writing/{category}/{date:%Y}/{date:%m}/{date:%d}/{slug}/"
+ARTICLE_SAVE_AS = "writing/{category}/{date:%Y}/{date:%m}/{date:%d}/{slug}/index.html"
 
 # Blogroll
 LINKS = ()
@@ -36,33 +37,33 @@ DISPLAY_PAGES_ON_MENU = True
 
 # Social widget
 SOCIAL = (
-    ('GitHub', 'https://github.com/bryanwweber'),
-    ('ORCID', 'https://orcid.org/0000-0003-0815-9270'),
-    ('Google Scholar', 'https://scholar.google.com/citations?user=48LLBNMAAAAJ&hl=en'),
-    ('LinkedIn', 'https://www.linkedin.com/in/bryanwweber'),
+    ("GitHub", "https://github.com/bryanwweber"),
+    ("ORCID", "https://orcid.org/0000-0003-0815-9270"),
+    ("Google Scholar", "https://scholar.google.com/citations?user=48LLBNMAAAAJ&hl=en"),
+    ("LinkedIn", "https://www.linkedin.com/in/bryanwweber"),
     # ('Mendeley', 'https://www.mendeley.com/profiles/bryan-w-weber/'),
     # ('Academia.edu', 'https://uconn.academia.edu/BryanWWeber'),
-    ('Zotero', 'https://www.zotero.org/darthbith'),
-    ('arXiv', 'https://arxiv.org/a/weber_b_1.html'),
+    ("Zotero", "https://www.zotero.org/darthbith"),
+    ("arXiv", "https://arxiv.org/a/weber_b_1.html"),
 )
 
 # Static Files
 STATIC_PATHS = [
-    'images',
-    'extra',
-    'files',
+    "images",
+    "extra",
+    "files",
 ]
 
-GITHUB_URL = 'https://github.com/bryanwweber/bryanwweber.com'
+GITHUB_URL = "https://github.com/bryanwweber/bryanwweber.com"
 
 EXTRA_PATH_METADATA = {
-    'extra/extra.css': {'path': 'css/extra.css'},
-    'extra/robots.txt': {'path': 'robots.txt'},
-    'extra/favicon.ico': {'path': 'favicon.ico'},
-    'extra/htaccess': {'path': '.htaccess'},
-    'extra/keybase.txt': {'path': 'keybase.txt'},
+    "extra/extra.css": {"path": "css/extra.css"},
+    "extra/robots.txt": {"path": "robots.txt"},
+    "extra/favicon.ico": {"path": "favicon.ico"},
+    "extra/htaccess": {"path": ".htaccess"},
+    "extra/keybase.txt": {"path": "keybase.txt"},
     # 'extra/css/academicons.css': {'path': 'extra/css/academicons.css'},
-    'extra/css/academicons.min.css': {'path': 'extra/css/academicons.min.css'},
+    "extra/css/academicons.min.css": {"path": "extra/css/academicons.min.css"},
 }
 
 DEFAULT_PAGINATION = 9
@@ -71,11 +72,11 @@ DEFAULT_PAGINATION = 9
 # RELATIVE_URLS = True
 
 THEME = bulrush.PATH
-PLUGIN_PATHS = ['pelican-plugins']
-PLUGINS = ['assets', 'summary', 'md_inline_extension']
+PLUGIN_PATHS = ["pelican-plugins"]
+PLUGINS = ["assets", "summary", "md_inline_extension"]
 JINJA_ENVIRONMENT = bulrush.ENVIRONMENT
 JINJA_FILTERS = bulrush.FILTERS
-SUMMARY_END_MARKER = '<!--more-->'
+SUMMARY_END_MARKER = "<!--more-->"
 SUMMARY_USE_FIRST_PARAGRAPH = True
 
 MARKDOWN = {
@@ -92,18 +93,18 @@ MARKDOWN = {
 }
 
 MD_INLINE = {
-    '|-|': 'cv-position',
-    '|+|': 'cv-duty',
-    '|=|': 'cv-location',
-    '|a|': 'cv-award',
-    '|p|': 'papertitle',
-    '|u|': 'paperauthors',
-    '|j|': 'paperjournal',
-    '|d|': 'paperdoi',
-    '|c|': 'papercomment',
-    '|x|': 'paperarxiv',
-    '|f|': 'paperfigshare',
-    '|r|': 'paperref',
+    "|-|": "cv-position",
+    "|+|": "cv-duty",
+    "|=|": "cv-location",
+    "|a|": "cv-award",
+    "|p|": "papertitle",
+    "|u|": "paperauthors",
+    "|j|": "paperjournal",
+    "|d|": "paperdoi",
+    "|c|": "papercomment",
+    "|x|": "paperarxiv",
+    "|f|": "paperfigshare",
+    "|r|": "paperref",
 }
 
 TYPOGRIFY = True

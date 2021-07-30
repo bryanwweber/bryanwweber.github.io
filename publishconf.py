0fa955f2029b20f700be8b8e4c735311d6929f7f
Bryan Weber <bryan.w.weber@gmail.com>
2021-07-30 09:00:48 -0400

    Switch publish URL back to bryanwweber.com

--- publishconf.py
+++ publishconf.py
@@ -8,7 +8,7 @@ sys.path.append(os.curdir)
 from pelicanconf import *
 
 # If your site is available via HTTPS, make sure SITEURL begins with https://
-SITEURL = "https://bryanwweber.github.io"
+SITEURL = "https://bryanwweber.com"
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = None
